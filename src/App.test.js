a65ce12da146fd1433c1f498e3594bcb4595fb10
AviramV <aviramv2@gmail.com>
2022-03-30 18:55:50 +0300

    Add render test for App component

--- src/App.test.js
+++ src/App.test.js
@@ -4,12 +4,22 @@ import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Posts', () => {
   const { getByText } = render(
     <Provider store={store}>
       <App />
     </Provider>
   );
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
+  expect(getByText(/Posted By/i)).toBeInTheDocument();
+});
+
+test('renders Loading', () => {
+  const { getByText } = render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+
+  expect(getByText(/Loading.../i)).toBeInTheDocument();
 });
