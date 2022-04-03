916d07bcd0e93e0ce7bc91b13853e0fa80431dfc
AviramV <aviramv2@gmail.com>
2022-04-03 13:54:51 +0300

    Add posts slice

--- src/Components/posts/Posts.js
+++ src/Components/posts/Posts.js
@@ -1,23 +1,36 @@
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { selectLoading, selectPosts, fetchPosts } from './postsSlice';
 import Post from '../post/Post';
+import Loader from '../loader/Loader';
 import './Posts.css'
 
 function Posts() {
-    return (
+    const isLoading = useSelector(selectLoading);
+    const posts = useSelector(selectPosts);
+    const { hasError } = useSelector(state => state.posts);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchPosts());
+      }, [dispatch]);
+
+    return isLoading ? <Loader /> : hasError ? <code>Couldn't laod posts, please try again later</code> :
+    (
         <div className="posts">
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
+        {posts.map((post) => <Post props={post.data}
+                                key={post.data.id}
+                                content={post.data.title}
+                                author={post.data.author}
+                                postTime={post.data.created_utc}
+                                url={post.data.url}
+                                commentsAmount={post.data.num_comments}
+                                votes={post.data.ups - post.data.downs}
+                                isVideo={post.data.is_video}
+                                isSelf={post.data.is_self}
+                                domain={post.data.domain}
+                                hint={post.data.post_hint}
+                            />)}
         </div>
     )
 }
---
+++ src/Components/posts/postsSlice.js
@@ -0,0 +1,46 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+
+
+const initialState = {
+    posts: [], // Set to reddit home page posts, using the API.
+    loading: false,
+    hasError: false,
+}; 
+
+export const fetchPosts = createAsyncThunk(
+    'posts/fetchPosts',
+    async () => {
+      const response = await fetch('https://www.reddit.com/.json');
+      // The value we return becomes the `fulfilled` action payload
+      const json = await response.json();
+      const posts = json.data.children;
+      return posts;
+    }
+  );
+
+const postsSlice = createSlice({
+    name: 'posts',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+          .addCase(fetchPosts.rejected, (state) => {
+            state.loading = false;
+            state.hasError = true;
+          })
+          .addCase(fetchPosts.pending, (state) => {
+            state.loading = true;
+            state.hasError = false;
+          })
+          .addCase(fetchPosts.fulfilled, (state, action) => {
+            state.loading = false;
+            state.hasError = false;
+            state.posts = action.payload;
+          });
+      },
+});
+
+export const selectLoading = (state) => state.posts.loading;
+export const selectPosts = (state) => state.posts.posts;
+export default postsSlice.reducer;
