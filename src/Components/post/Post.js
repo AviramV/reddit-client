8d000c4ae75fbbe3e7d4bf9a23081dd78d50ae2c
AviramV <aviramv2@gmail.com>
2022-04-07 18:12:07 +0300

    Add support for iframes

--- src/Components/post/Post.js
+++ src/Components/post/Post.js
@@ -3,6 +3,7 @@ import './Post.css';
 import comments from '../../icons/comments.svg';
 import arrowUp from '../../icons/arrowUp.svg';
 import arrowDown from '../../icons/arrowDown.svg';
+import React from 'react';
 
 function Post({
     content,
@@ -19,13 +20,19 @@ function Post({
     props
 }) {
 
+    const { is_gallery } = props;
+
     // Format from Epoch to actual date
     const timeConvert = (time) => {
         const date = new Date(time*1000);
         return formatRelativeDate(date);
     }
     // Remove interfering charachters from URL string in JSON object
-    const decodeURI = (uri) => uri.replace(/amp;/g, '')
+    const purgeString = (string) => {
+       return string.replace(/amp;/g, '')
+       .replaceAll("&lt;", '<')
+       .replaceAll("&gt;", '>');
+    }
 
     // Return correct element according to type of media in post (link/image/video)
     const mediaComponent = () => {
@@ -34,23 +41,39 @@ function Post({
                 return <a className="post-link" href={url}>{domain}</a>;
         
             case "image":
-                const imageURL = decodeURI(props.preview.images[0].source.url);
+                const isGif = props.preview.images[0].variants?.gif?.source;
+                const imageSource = props.preview.images[0].source;
                 const imageResolutions = props.preview.images[0].resolutions;
-                return <img 
+                if (!isGif) {
+                    return <a target="_blank" rel="noreferrer" href={url}>
+                            <img 
+                            id="main-image" 
+                            loading="lazy"
+                            decoding="auto"
+                            alt="" 
+                            src={imageSource.url}
+                            
+                            srcSet={imageResolutions.map(el => `${purgeString(el.url)} ${el.width}w`)}
+                            sizes="(min-width: 960px) 40vw ,100vw" />
+                        </a>
+                }
+                return <a target="_blank" rel="noreferrer" href={url}>
+                        <img 
                         id="main-image" 
                         loading="lazy"
-                        width="800"
                         decoding="auto"
                         alt="" 
                         src={url} 
-                        srcSet={imageResolutions.map(el => `${decodeURI(el.url)} ${el.width}w`)}
-                        sizes="(min-width: 960px) 75vw ,100vw" />
+                        width={isGif.width}
+                        height={isGif.height}
+                        />
+                    </a>
         
             case "hosted:video":
-                const posterImg = decodeURI(props.preview.images[0].source.url);
-                const videoURL = decodeURI(props.media.reddit_video.fallback_url);
-                const dashURL = decodeURI(props.media.reddit_video.dash_url);
-                const hlsURL = decodeURI(props.media.reddit_video.hls_url);
+                const posterImg = purgeString(props.preview.images[0].source.url);
+                const videoURL = purgeString(props.media.reddit_video.fallback_url);
+                const dashURL = purgeString(props.media.reddit_video.dash_url);
+                const hlsURL = purgeString(props.media.reddit_video.hls_url);
                 return (
                     <video 
                         autoPlay
@@ -65,6 +88,11 @@ function Post({
                             Sorry, your browser doesn't support embedded videos.
                     </video>
                 )
+
+                case "rich:video":
+                    const embedString = decodeURIComponent(props.media.oembed.html);
+                    const embedHtml = purgeString(embedString);
+                    return <div dangerouslySetInnerHTML={{__html: embedHtml}}></div>
             default:
                 break;
         }
@@ -75,12 +103,18 @@ function Post({
             <div className="post-header">
                 {/* <img src="https://i.imgur.com/aEcJUFK.png" alt="" /> */}
                 <p className="post-info">
-                Posted by <b>{author}</b>,  {timeConvert(postTime)}
+                Posted by <b>{author}</b>, {timeConvert(postTime)}
                 </p>
             </div>
-            <h3>{content}</h3>
+            <h3>{purgeString(content)}</h3>
             {!isSelf &&
                 <div className="media-container">
+                    {is_gallery && 
+                    <a target='_blank' 
+                       rel="noreferrer" 
+                       href={url}>
+                       View Gallery
+                    </a>}
                     {mediaComponent()}
                 </div>
             }
--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import postsReducer from '../Components/posts/postsSlice';
+import commentsReducer from '../Components/comments/commentsSlice';
+import searchTermReducer from '../Components/searchBar/searchBarSlice';
+import categoryFilterReducer from '../Components/filter/filterSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    posts: postsReducer,
+    comments: commentsReducer,
+    searchTerm: searchTermReducer,
+    categoryFilter: categoryFilterReducer
   },
 });
