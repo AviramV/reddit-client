c0c91014c2081cba1d6865a7fd6a07645cc157cf
AviramV <aviramv2@gmail.com>
2022-04-16 22:13:13 +0300

    Replace datalist element with select and Add loading category posts on select

--- src/Components/filter/Filter.js
+++ src/Components/filter/Filter.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { selectCategories, fetchCategories } from './filterSlice';
+import { fetchPosts } from '../posts/postsSlice';
 
 function Filter() {
     const categories = useSelector(selectCategories);
@@ -10,21 +11,25 @@ function Filter() {
         dispatch(fetchCategories());
     }, [])
 
+    const handleChange = ({ target }) => {
+        if (target.value === 'Select category...') return
+        const URL = `https://www.reddit.com${target.value}.json`
+        dispatch(fetchPosts(URL))
+    }
+
     return (
     <form>
-        <input
-            className="header-input"
-            list="categories"
-            placeholder="Choose Category"
-        />
-        <datalist id="categories">
+        <select id="categories" className="header-input" onChange={handleChange}>
+            <option>Select category...</option>
             {
                 categories.map((category, index) => {
-                    const name = category.data.display_name_prefixed;
-                    return <option key={index} value={name}/>
+                    const name = category.data.display_name;
+                    const icon = category.data.icon_img;
+                    const { url } = category.data;
+                    return <option key={index} value={url} type="text">{name}</option>
                 })
             }
-        </datalist>
+        </select>
     </form>
     );
 }
