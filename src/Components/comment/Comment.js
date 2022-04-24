3896d4aee4cf324b98fee6764767db2fa68568bb
AviramV <aviramv2@gmail.com>
2022-04-24 13:23:10 +0300

    Add one level of replies to comments

--- src/Components/comment/Comment.js
+++ src/Components/comment/Comment.js
@@ -8,7 +8,17 @@ import './Comment.css'
 
 function Comment({ body, score, author, created, replies }) {
     const [showReplies, setShowReplies] = useState(false);
-    const haveReplies = Array.isArray(replies);
+
+    // Replies are received in an array, no replies comes in as an empty string.
+    // Check if a comment has replies
+    const hasReplies = Array.isArray(replies);
+    
+    // Filter out any element that's not a reply
+    const filteredReplies = () => {
+        if (!hasReplies) return;
+        return replies.filter(reply => reply.kind === 't1');
+    }
+
     return (
         <div className="comment">
             <div className="comment-header">
@@ -16,10 +26,10 @@ function Comment({ body, score, author, created, replies }) {
             </div>
             <div className="comment-content" dangerouslySetInnerHTML={{__html: purgeString(body)}}></div>
             <div className="comment-footer">
-                { haveReplies && 
+                { hasReplies && filteredReplies().length > 0 &&
                     <div className="comment-replies" onClick={() => setShowReplies(!showReplies)}>
                         <img src={comments} alt="" />
-                        <p>{formatCompactNumber(replies.length)} {replies.length > 1 ? "Replies" : "Reply"}</p>
+                        <p>{formatCompactNumber(filteredReplies().length)} {filteredReplies().length > 1 ? "Replies" : "Reply"}</p>
                     </div>
                 }
                 <div className="comment-votes">
@@ -28,7 +38,17 @@ function Comment({ body, score, author, created, replies }) {
                     <img src={arrowDown} alt="" />
                 </div>
             </div>
-                {showReplies && <div className="replies">reply</div>}
+                {
+                    showReplies && 
+                    <div className="replies">
+                        {
+                    filteredReplies().map(reply => {
+                        const { id, body, score, author, created } = reply.data;
+                        return <Comment key={id} body={body} score={score} author={author} created={created} />
+                    }) 
+                        }
+                    </div>
+                }
         </div>
     );
 }
